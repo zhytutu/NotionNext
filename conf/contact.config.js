62471dadf612c1aec0284fd9a39ffea8fdf30c48
hebao <zhytutu@outlook.com>
2026-01-10 21:13:54 +0800

    删除底部微博图标

--- conf/contact.config.js
+++ conf/contact.config.js
@@ -9,7 +9,7 @@ module.exports = {
         unescape(encodeURIComponent(process.env.NEXT_PUBLIC_CONTACT_EMAIL))
       )) ||
     '', // 邮箱地址 例如mail@tangly1024.com
-  CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || 'https://weibo.com/u/3904842495', // 你的微博个人主页
+  CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
   CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || '', // 你的twitter个人主页
   CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || '', // 你的github个人主页 例如 https://github.com/tangly1024
   CONTACT_TELEGRAM: process.env.NEXT_PUBLIC_CONTACT_TELEGRAM || '', // 你的telegram 地址 例如 https://t.me/tangly_1024
--- themes/heodiy/components/PostCopyright.js
+++ themes/heodiy/components/PostCopyright.js
@@ -40,10 +40,10 @@ export default function PostCopyright() {
             {path}
           </a>
         </li>
-        <li>
+        {/*/<li>
           <strong className='mr-2'>{locale.COMMON.COPYRIGHT}:</strong>
           {locale.COMMON.COPYRIGHT_NOTICE}
-        </li>
+        </li>*/}
         {siteConfig('HEO_ARTICLE_NOT_BY_AI', false, CONFIG) && (
           <li>
             <NotByAI />
