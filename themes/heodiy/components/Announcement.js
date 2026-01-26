8236fcf2a81bf83799b260221ffa6327bbd9f7b7
zhytutu <zhytutu@outlook.com0>
2026-01-26 23:58:00 +0800

    修改手机端公告底色为#4f65f0，与网页版一致

--- themes/heodiy/components/Announcement.js
+++ themes/heodiy/components/Announcement.js
@@ -7,7 +7,7 @@ const Announcement = ({ post, className }) => {
     return (
       <div className={className}>
         {post && (
-          <div id='announcement-content' className='bg-white dark:bg-[#1e1e1e]'>
+          <div id='announcement-content' className='bg-[#4f65f0] dark:bg-yellow-600'>
             <NotionPage post={post} />
           </div>
         )}
