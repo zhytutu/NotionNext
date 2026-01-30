8fe5b93d67bbc02df54dc6090c08b690521116fa
hebao <zhytutu@outlook.com>
2026-01-30 12:45:28 +0000

    Revert "手机公告高度适应"
    This reverts commit 423e645a390d06891d8c8dee9a2ca9a5146ac52c.

--- themes/heodiy/components/Announcement.js
+++ themes/heodiy/components/Announcement.js
@@ -6,9 +6,9 @@ const NotionPage = dynamic(() => import('@/components/NotionPage'))
 const Announcement = ({ post, className, showGreetings = false }) => {
   if (post?.blockMap) {
     return (
-      <div>
+      <div className={className}>
         {post && (
-          <div id='announcement-content' className={`${className || 'bg-[#4f65f0] dark:bg-[#1e1e1e]'} rounded-xl p-3 overflow-hidden`}>
+          <div id='announcement-content' className='bg-[#4f65f0] dark:bg-[#1e1e1e] rounded-xl'>
             {showGreetings && <GreetingsWords />}
             <NotionPage post={post} />
           </div>
